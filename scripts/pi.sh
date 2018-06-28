fde0d6dcb2d526e3321275b52a136bbf4e2a00ee
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2018-06-28 16:59:36 -0400

    raspberry pi: centos 7 installation

---
+++ scripts/pi.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+yum -y update
+touch /.rootfs-repartition
+systemctl reboot
+yum -y install yum-utils
+yum -y groupinstall development
+yum -y install bash-completion
+yum -y install lvm2*
+yum -y install tmux vim wget
