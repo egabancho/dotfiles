bbe8afcc3b7d5715b546c2bf4f17941650bf1b87
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2018-07-02 17:12:11 -0400

    brew: update with some random stuff

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -9,8 +9,7 @@ fi
 
 # Install Bash 4.
 brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
+brew install bash-completion
 # We installed the new shell, now we have to activate it
 echo "Adding the newly installed shell to the list of allowed shells"
 # Prompts for password
@@ -22,5 +21,41 @@ chsh -s /usr/local/bin/bash
 brew install python
 brew install python3
 
+# Base stuff
+
 # Install `wget` with IRI support.
 brew install wget --with-iri
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`
+brew install findutils --with-default-names
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+brew install tmux
+brew install git git-extras ghi hub
+
+# Editors
+brew install emacs-plus --with-mailutils --with-no-titlebar
+brew tap d12frosted/emacs-plus
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+brew tap caskroom/fonts && brew cask install font-source-code-pro
+brew install vim
+curl https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim --create-dirs -o ~/.vim/colors/solarized.vim
+
+# Linux tools and handy utils
+# https://github.com/brona/iproute2mac
+brew install iproute2mac
+brew install ispell
+brew install ag ack
+brew install colordiff
+brew install tree
+# File converter
+brew install pandoc
+# Network utils
+brew install nmap
+
+brew cleanup
--- scripts/cask.sh
+++ scripts/cask.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+
+brew cask install 1password
+brew cask install dropbox
+brew cask install firefox
+cask install chromedriver
+brew cask install appcleaner
+brew cask install iterm2
+brew cask install spectacle
+brew cask install whatsapp
+brew cask install duet
+brew cask install gitter
+brew cask install spotify
+brew cask install gpgtools
+
+# Install developer friendly quick look plugins
+# see https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook suspicious-package
