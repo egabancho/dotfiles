dac96bc799fe8dd3ceb6d2fd788420584f713b45
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2018-07-03 12:10:06 -0400

    pi: update arch installation

--- scripts/pi_arch.sh
+++ scripts/pi_arch.sh
@@ -2,10 +2,12 @@
 
 su -
 
+passwd
+
 # Initialize the pacman keyring
 pacman-key --init
 # Populate the Arch Linux ARM package signing keys
-pacman-key --populate archlinuxarmpacman-key --populate archlinuxarm
+pacman-key --populate archlinuxarm
 
 pacman -Syu
 
@@ -13,15 +15,19 @@ rm /etc/localtime
 ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
 timedatectl set-ntp true
 
-pacman -Sy sudo
-pacman -S xorg xorg-server
-pacman -S xterm make
-pacman -Sy vim neovim python-neovim
-curl -sLf https://spacevim.org/install.sh | bash
-pacman -S emacs
+pacman -S --noconfirm sudo
+pacman -S --noconfirm base-devel
+pacman -S --noconfirm htop tigervnc python
+pacman -S --noconfirm xfce4
+pacman -S --noconfirm xorg xorg-xinit xorg-server xterm
+pacman -S --noconfirm alsa-utils alsa-firmware alsa-lib alsa-plugins
+pacman -S --noconfirm vim neovim python-neovim
+pacman -S --noconfirm  emacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-pacman -S tmux git hub bash-completion
-pacman -S docker
+pacman -S --noconfirm tmux git hub bash-completion
+pacman -S --noconfirm docker
 systemctl start docker.service
 systemctl enable docker.service
-# gpasswd -a egabancho docker
+
+useradd -m -G wheel, docker -s /bin/bash egabancho
+passwd egabancho
