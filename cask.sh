f86c9ff94ae59f360d11198f7a595e67ba614b01
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2018-12-24 18:55:17 +0100

    cask: add java :(

--- cask.sh
+++ cask.sh
@@ -13,6 +13,7 @@ brew cask install google-chrome
 brew cask install gpg-suite
 brew cask install iina
 brew cask install iterm2
+brew cask install java
 brew cask install kap
 brew cask install karabiner-elements
 brew cask install keybase
